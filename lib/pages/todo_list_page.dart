f377b55a7d27fb827622829029140993daf385d1
Fábio Teixeira <fabio@fabio.vet.br>
2022-03-07 23:48:05 -0300

    Preenchendo a lista dinamicamente

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/todo_list_item.dart';
+
 class TodoListPage extends StatefulWidget {
   TodoListPage({Key? key}) : super(key: key);
 
@@ -14,83 +16,83 @@ class _TodoListPageState extends State<TodoListPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: todoController,
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Adicione uma tarefa aqui',
-                          hintText: 'Ex.: Estudar Flutter'),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      String text = todoController.text;
-                      setState(() {
-                        tarefas.add(text);
-                      });
-                      todoController.clear();
-                    },
-                    style: ElevatedButton.styleFrom(
-                        primary: const Color(0xff00d7f3),
-                        padding: const EdgeInsets.all(14)),
-                    child: const Icon(
-                      Icons.add,
-                      size: 30,
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Flexible(
-                child: ListView(
-                  shrinkWrap: true,
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
                   children: [
-                    for (String tarefa in tarefas)
-                      ListTile(
-                        title: Text(tarefa),
-                        onTap: () {
-                          print('Tarefa: $tarefa');
-                        },
+                    Expanded(
+                      child: TextField(
+                        controller: todoController,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Adicione uma tarefa aqui',
+                            hintText: 'Ex.: Estudar Flutter'),
                       ),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        String text = todoController.text;
+                        setState(() {
+                          tarefas.add(text);
+                        });
+                        todoController.clear();
+                      },
+                      style: ElevatedButton.styleFrom(
+                          primary: const Color(0xff00d7f3),
+                          padding: const EdgeInsets.all(14)),
+                      child: const Icon(
+                        Icons.add,
+                        size: 30,
+                      ),
+                    )
                   ],
                 ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Row(
-                children: [
-                  const Expanded(
-                    child: Text('Você possui 0 tarefas pendentes'),
+                const SizedBox(
+                  height: 16,
+                ),
+                Flexible(
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      for (String tarefa in tarefas)
+                        TodoListItem(
+                          title: tarefa,
+                        ),
+                    ],
                   ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Limpar tudo'),
-                    style: ElevatedButton.styleFrom(
-                        primary: const Color(0xff00d7f3),
-                        padding: const EdgeInsets.all(14)),
-                  ),
-                ],
-              )
-            ],
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                          'Você possui ${tarefas.length} tarefas pendentes'),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Limpar tudo'),
+                      style: ElevatedButton.styleFrom(
+                          primary: const Color(0xff00d7f3),
+                          padding: const EdgeInsets.all(14)),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
